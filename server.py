928709844b4ce242e4608fafad7300a6f73c0fd2
MykySan <myky.ivashchenko@gmail.com>
2025-10-06 04:59:26 -0400

    add some docstrings to server.py

--- server.py
+++ server.py
@@ -1,10 +1,19 @@
+''' Executing this function initiates the application of sentiment
+    analysis to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
 from flask import Flask, request, render_template
-from EmotionDetection.emotion_detection import emotion_detector  # ваш модуль
+from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
 @app.route("/emotionDetector", methods=["GET", "POST"])
-def emotionDetector():
+def emotion_detector_func():
+    ''' This code receives the text from the HTML interface and 
+        runs emotion detection over it using emotion_detector()
+        function. The output returned shows the label and its confidence 
+        score for the provided text.
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
     if response.get('dominant_emotion') is None:
@@ -21,6 +30,9 @@ def emotionDetector():
 
 @app.route("/")
 def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
